4507a649f6098cf76a898c2bf2617ef063820dbb
highlander <lxrbckl@gmail.com>
2022-05-23 02:43:18 -0500

    introduction of PyPi package

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
 
-    version = '0.2',
+    version = '0.4',
     name = 'lxRbckl',
     author = 'Alexander Arbuckle',
     author_email = 'lxrbckl@gmail.com',
