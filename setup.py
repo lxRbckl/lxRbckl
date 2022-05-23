874598fc1b23834b55282002929c0e8e5e4a2ac7
highlander <lxrbckl@gmail.com>
2022-05-23 01:42:26 -0500

    introduction of PyPi package

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+
+setuptools.setup(
+
+    name = 'lxRbckl',
+    version = '0.1',
+    author = 'Alexander Arbuckle',
+    author_email = 'lxrbckl@gmail.com',
+    description = 'Layers of abstraction to increase readability and productivity in lxRbckl projects.',
+    packages = setuptools.find_packages(),
+    classifiers = [
+
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+
+    ]
+
+)
