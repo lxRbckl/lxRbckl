3c40801502003cb47886288d901c4d615762de73
ala2q6 <lxrbckl@protonmail.com>
2022-12-03 12:40:52 -0600

    introduction to question 3 on leetcode

---
+++ leetcode/3.py
@@ -0,0 +1,14 @@
+class Solution:
+
+    def lengthOfLongestSubstring(self, s: str) -> int:
+
+        i = 0
+        l = []
+        for j in s:
+
+            if (j in l): l = [*l[(l.index(j) + 1) :], j]
+            else: l.append(j)
+
+            if (len(l) > i): i = len(l)
+
+        return i
