87e7618a4a2fb8f0385e5e4d5c184cb7d4f4cad9
lxrbckl <lxrbckl@gmail.com>
2023-09-25 10:17:37 -0500

    introducing package to python ecosystem

--- leetcode/292.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-
-
-    def canWinNim(self, n: int) -> bool:
-
-        if (n % 4 == 0): return False
-        else: return True
--- leetcode/9.py
+++
@@ -1,5 +0,0 @@
-class Solution:
-
-
-    def isPalindrome(self, x: int) -> bool: return str(x)[::-1] == str(x)
-        
