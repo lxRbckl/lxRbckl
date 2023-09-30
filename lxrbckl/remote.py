f14097068a655184fbea7fb21bf8ebf09e0f4a66
lxRbckl <lxrbckl@gmail.com>
2023-09-30 12:42:07 -0500

    githubSet

--- lxrbckl/remote.py
+++ lxrbckl/remote.py
@@ -11,11 +11,36 @@ from json import loads, dumps
 # >
 
 
-def githubSet():
+def githubSet(
+   
+   pData,
+   pFile: str,
+   pGithub: object,
+   pRepository: str,
+   pBranch: str = 'main',
+   pMessage: str = 'Automated Commit'
+   
+):
    '''  '''
    
-   pass
+   # get repository <
+   # format the data <
+   # get content from repository <
+   repository = pGithub.get_repo(pRepository)
+   data = str(pData).replace('\'', '\"').replace('None', 'null')
+   content = repository.get_contents(path = pFile, ref = pBranch)
 
+   # >
+
+   repository.update_file(
+
+      content = data,
+      branch = pBranch,
+      sha = content.sha,
+      message = pMessage,
+      path = content.path
+
+   )
 
 def githubGet():
    '''  '''
