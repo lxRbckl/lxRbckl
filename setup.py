289c119b04515419e801da038e7fc414e52d42df
lxRbckl <lxrbckl@gmail.com>
2023-10-05 14:58:34 -0500

    version update

--- setup.py
+++ setup.py
@@ -3,19 +3,12 @@ from setuptools import setup, find_packages
 
 setup(
 
-    version = '1.0',
     name = 'lxRbckl',
+    version = '2.0.0',
+    packages = find_packages(),
     author = 'Alexander Arbuckle',
     author_email = 'lxrbckl@gmail.com',
-    packages = find_packages(),
-    url = 'https://github.com/lxRbckl/lxRbckl',
-    description = 'Layers of abstraction to increase readability and productivity in lxRbckl projects.',
-    classifiers = [
-
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent'
-
-    ]
+    url = 'https://github.com/lxRbckl/lxRbckl/tree/python',
+    description = 'CRUD tools for local and remote files, enhancing readability and abstraction in lxRbckl projects.'
 
 )
