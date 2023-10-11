b6a9666ca62d44bed7568658a23e3f8502fb794d
lxRbckl <lxrbckl@gmail.com>
2023-10-11 16:09:01 -0500

    Update

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 setup(
 
     name = 'lxRbckl',
-    version = '2.0.1',
+    version = '3.0.1',
     packages = find_packages(),
     author = 'Alexander Arbuckle',
     author_email = 'lxrbckl@gmail.com',
