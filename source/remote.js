b5b7778d231a92de25b5f7c4dbc8b3982f289566
lxrbckl <lxrbckl@gmail.com>
2023-11-07 15:58:11 -0600

    Update

--- source/remote.js
+++ source/remote.js
@@ -1,24 +1,100 @@
 // import <
 const axios = require('axios');
+const {Octokit} = require('@octokit/rest');
 
 // >
 
 
-async function axiosGet({pURL}) {
+async function githubGet({
 
+   pPath,
+   pOwner,
+   pGithub,
+   pBranch,
+   pRepository,
+   pEncoding = 'base64',
+   pErrorMessage = 'Could not find file.'
+
+}) {
+
+   // try (if file exists) <
+   // except (then file does not exist) <
    try {
 
-      const extension = pURL.split('.').slice(-1);
-      const response = (await axios.get(pURL)).data;
+      const response = await pGithub.repos.getContent({
 
-      return {
+         path : pPath,
+         ref : pBranch,
+         owner : pOwner,
+         repo : pRepository
 
-         'txt' : () => {return response;},
-         'json' : () => {return JSON.parse(JSON.stringify(response));}
+      });
 
-      }[extension]();
-      
-   } catch (error) {return 'Data could not be loaded.';}
+      return Buffer.from(response.data.content, pEncoding).toString();
+
+   } catch (error) {return pErrorMessage;}
+
+   // >
+
+}
+
+
+async function githubUpdate({
+
+   pPath,
+   pOwner,
+   pGithub,
+   pBranch,
+   pContent,
+   pRepository,
+   pEncoding = 'base64',
+   pMessage = 'This is an automated action.',
+   pErrorMessage = 'Could not create or update file.'
+
+}) {
+
+   // try (if exists) <
+   // except (then does not exist) <
+   try {
+
+      const content = JSON.stringify(pContent);
+      const response = await pGithub.repos.getContent({
+
+         path : pPath,
+         ref : pBranch,
+         owner : pOwner,
+         repo : pRepository
+
+      });
+
+      await pGithub.repos.createOrUpdateFileContents({
+
+         path : pPath,
+         ref : pBranch,
+         owner : pOwner,
+         repo : pRepository,
+         message : pMessage,
+         sha : response.data.sha,
+         content : Buffer.from(content).toString(pEncoding)
+
+      });
+
+   } catch (error) {console.log(error); return pErrorMessage;}
+
+   // >
+
+}
+
+
+async function axiosGet({
+   
+   pURL,
+   pErrorMessage = 'Data could not be loaded.'
+
+}) {
+
+   try { return (await axios.get(pURL)).data}   
+   catch (error) {return pErrorMessage;}
 
 }
 
@@ -26,7 +102,9 @@ async function axiosGet({pURL}) {
 // export <
 module.exports = {
 
-   axiosGet
+   axiosGet,
+   githubGet,
+   githubUpdate
 
 };
 
