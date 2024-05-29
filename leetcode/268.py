30dfedc9f74273e0a912063763ba76b80849bd45
Alex Arbuckle <84994842+lxRbckl@users.noreply.github.com>
2024-05-28 22:31:13 -0500

    Delete leetcode directory

--- leetcode/268.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-
-
-    def missingNumber(self, nums: List[int]) -> int:
-
-        x = set(nums)
-        y = set(i for i in range(max(nums) + 2))
-
-        return sorted(list(y - x))[0]
--- leetcode/4.py
+++
@@ -1,9 +0,0 @@
-from statistics import median
-
-
-class Solution:
-
-
-    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-
-        return median(sorted(nums1 + nums2))
